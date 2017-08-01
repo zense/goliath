c4b68b2ed12400b9b2bc8d21894dc67541548a3a
Gaurav Koley <arkokoley@live.in>
2017-08-01 18:53:42 +0530

    Integrated graphQL client - Apollo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import { browserHistory } from 'react-router';
 
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
+import {
+  ApolloClient,
+  gql,
+  graphql,
+  ApolloProvider,
+} from 'react-apollo';
 
-ReactDOM.render(<Routes history={ browserHistory } />, 
-    document.getElementById('root'));
+const client = new ApolloClient();
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Routes history={ browserHistory } />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
